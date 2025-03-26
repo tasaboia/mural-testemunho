568b644c86454eefc0c881161ef14fe23e568e7a
dnms.tech <tech.dnms@gmail.com>
2025-03-26 13:43:43 +0000

    teste de porta

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { Logger } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const logger = new Logger('Bootstrap');
+  const logger = new Logger("Bootstrap");
 
-  app.setGlobalPrefix('api');
-  
+  app.setGlobalPrefix("api");
+
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3001',
+    origin: process.env.FRONTEND_URL || "http://localhost:3001",
     credentials: true,
   });
 
-  const PORT = process.env.PORT || 3000;
-  await app.listen(PORT);
-  logger.log(`🚀 Backend rodando em http://localhost:${PORT}`);
+  await app.listen(process.env.PORT);
+  logger.log(`🚀 Backend rodando !`);
 }
 bootstrap();
